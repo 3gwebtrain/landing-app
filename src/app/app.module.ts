845dc24d30727e9dbed245eeb36d4534c321a4f3
3gwebtrain <3gwebtrain@gmail.com>
2021-12-22 09:38:41 +0530

    ngrx/data implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import {AppComponent} from './app.component';
 import {WeatherModule} from './weather/weather.module';
 import {NotificationsModule} from './notifications/notifications.module';
 import {NewsApiModule} from './news-api/news-api.module';
+import {StoreModule} from '@ngrx/store';
+import {DefaultDataServiceConfig, EntityDataModule} from '@ngrx/data';
+import {entityConfig} from './entity-metadata';
+import {EffectsModule} from '@ngrx/effects';
+
+const customDataServiceConfig: DefaultDataServiceConfig = {
+  root: 'https://api.openweathermap.org/data/2.5/',
+}
 
 @NgModule({
   declarations: [
@@ -16,9 +24,12 @@ import {NewsApiModule} from './news-api/news-api.module';
     HttpClientModule,
     WeatherModule,
     NotificationsModule,
-    NewsApiModule
+    NewsApiModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    EntityDataModule.forRoot(entityConfig)
   ],
-  providers: [],
+  providers: [{provide: DefaultDataServiceConfig, useValue: customDataServiceConfig}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/weather/forecast/forecast.component.ts
+++ src/app/weather/forecast/forecast.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
-import {ForecastService} from '../forecast.service';
+import {ForecastService, OpenWeatherResponse} from '../forecast.service';
 
 
 @Component({
@@ -8,12 +8,18 @@ import {ForecastService} from '../forecast.service';
   templateUrl: './forecast.component.html',
   styleUrls: ['./forecast.component.css']
 })
-export class ForecastComponent {
+export class ForecastComponent implements OnInit {
 
   forecast$: Observable<{dateString: string; temp: number;}[]>;
+  newForecast$: Observable<OpenWeatherResponse[]>;
 
-  constructor(forecastService: ForecastService) {
-    this.forecast$ = forecastService.getForecast();
+  constructor(private forecastService: ForecastService) {
+    this.forecast$ = this.forecastService.getForecast();
+    this.newForecast$ = this.forecastService.entities$;
+  }
+
+  ngOnInit() {
+    this.forecastService.getAll().subscribe((data: any) => this.newForecast$ = data);
   }
 
 }
